43919ed7731d55141286e924dc94e993b9f4f581
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-09-05 09:57:14 +0700

    add more example

--- demo.go
+++ demo.go
@@ -10,4 +10,7 @@ func main() {
 	fmt.Println(bahttext.Words(1234.56))                  // หนึ่งพันสองร้อยสามสิบสี่บาทห้าสิบหกสตางค์
 	fmt.Println(bahttext.Words(-100))                     // ลบหนึ่งร้อยบาทถ้วน
 	fmt.Println(bahttext.WordsFromString("1,000,000.01")) // หนึ่งล้านบาทหนึ่งสตางค์
+
+	fmt.Println(bahttext.Words(1_000_000.01))     // หนึ่งล้านบาทหนึ่งสตางค์
+	fmt.Println(bahttext.Words(1_000_000_000.01)) // หนึ่งล้านบาทหนึ่งสตางค์
 }
