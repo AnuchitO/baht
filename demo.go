70c6bc7ecd60a345029b8eb6b5d6d50a5639437a
AnuchitO <3372362+AnuchitO@users.noreply.github.com>
2025-09-05 09:53:57 +0700

    demo how to use bahttext lib

---
+++ demo.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/anuchito/bahttext"
+)
+
+func main() {
+	fmt.Println(bahttext.Words(1234.56))                  // หนึ่งพันสองร้อยสามสิบสี่บาทห้าสิบหกสตางค์
+	fmt.Println(bahttext.Words(-100))                     // ลบหนึ่งร้อยบาทถ้วน
+	fmt.Println(bahttext.WordsFromString("1,000,000.01")) // หนึ่งล้านบาทหนึ่งสตางค์
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/anuchito/baht
+
+go 1.25
+
+require github.com/anuchito/bahttext v1.0.1
